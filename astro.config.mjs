d512ea842f4ee5129413dba76685488a74f2bef6
Henry Cabral <hjcbr19@gmail.com>
2023-07-30 23:36:47 -0600

    changes netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,14 +5,12 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import netlify from '@astrojs/netlify/functions';
 
+// https://astro.build/config
 export default defineConfig({
   site: "https://enableu.ai",
-  integrations: [
-    tailwind(),
-    image({
-      serviceEntryPoint: "@astrojs/image/sharp",
-    }),
-    mdx(),
-    sitemap(),
-  ],
+  integrations: [tailwind(), image({
+    serviceEntryPoint: "@astrojs/image/sharp"
+  }), mdx(), sitemap()],
+  output: "server",
+  adapter: netlify()
 });
